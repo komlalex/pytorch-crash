2f8c0488186e4ec40ed0ecfc156d8f5f8ee92dd1
Alexander Komla <kunalexander360@gmail.com>
2026-07-05 01:40:40 +0000

    Creating tensors

--- 00_fundamentals.py
+++ 00_fundamentals.py
@@ -26,3 +26,29 @@ TENSOR =   torch.tensor([[[1, 2, 3],
                           [4, 5, 6], 
                           [7, 8, 9]]]) 
 
+"""
+RANDOM TENSORS 
+
+Random tensors are important because the way many neural networks learn is that they start 
+with tensors full of random numbers and then adjust those random numbers 
+to better represent the data  
+"""
+# Create a random tensor  of shape (3, 4) 
+random_tensor = torch.rand(3, 4) 
+
+
+# Create a random tensor with similar shape to an image 
+random_image_tensor = torch.rand(size=(224, 224, 3)) # height, with, color channels (R, G, B)
+
+
+#Zeros and Ones
+zeros = torch.zeros(size=(3,4))  
+ones = torch.ones(size=(4,4)) 
+
+#Creating a range of tensors
+range_tensor = torch.arange(1, 10, 2) 
+
+# Tensors Like 
+one_to_ten = torch.tensor([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) 
+ten_zeros = torch.zeros_like(one_to_ten) 
+print(ten_zeros)
