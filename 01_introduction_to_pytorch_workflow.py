df1ee8957b4dee92aed4c8b6944597b0a5aa8335
Alexander Komla <kunalexander360@gmail.com>
2026-07-05 15:46:59 +0000

    PyTorch workflow

---
+++ 01_introduction_to_pytorch_workflow.py
@@ -0,0 +1,14 @@
+import torch 
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt   
+
+"""PyTorch Workflow
+Let's explore PyTorch end-to-end workflow 
+1. Prepare and Load Data 
+2. Build model 
+3. Fit model to data (training)
+4. Make predictions and evalute the model (inference) 
+5. Save and Load the model 
+6. Putting it all together 
+"""
