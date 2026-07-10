e25d5bfee8d6f7da9b3f1022f6e985ff36404e66
Alexander Komla <kunalexander360@gmail.com>
2026-07-10 01:04:23 +0000

    Download dataset

--- 06_computer_vision.py
+++ 06_computer_vision.py
@@ -1,8 +1,41 @@
+"""
+Computer vision librariesi in PyTorch 
+* torchvision 
+* torchvision.datasets 
+* torchvision.models 
+* torchvision.transforms
+* torch.utils.data.Dataset 
+* torch.utils.data.DataLoader""" 
+# Import torch
 import torch    
-from torch import nn 
+from torch import nn  
+
+# Import torchvision 
+import torchvision
+from torchvision import datasets 
+from torchvision import transforms 
+from torchvision.transforms import ToTensor
+
+# Import matplotlib 
 import matplotlib.pyplot as plt  
-import numpy as np 
-import math
+print(torchvision.__version__)
 
-X = torch.arange(-10, 10, 0.5) 
+"""Get the dataset 
+The dataset used is fashion MNIST from torchvision.datasets""" 
 
+# Setup training data 
+train_data = datasets.FashionMNIST(
+    root="data", 
+    train=True, 
+    download=True, 
+    transform=ToTensor(),
+    target_transform=None
+) 
+
+train_data = datasets.FashionMNIST(
+    root="data", 
+    train=False, 
+    download=True, 
+    transform=ToTensor(),
+    target_transform=None
+)
